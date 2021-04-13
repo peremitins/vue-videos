3bfd1f1a2c6dcec17ba314fce19574b64a7f5e64
nikolay_peremitin <peremitinns@mail.ru>
2021-04-13 21:05:05 +0300

    add project movie

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <router-view/>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+body {
+  background-color: #666;
+}
+a {
+  text-decoration: none;
+}
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0,0,0, .1);
+  h1 {
+    color: #fff;
+    font-size: 28px;
+    span {
+      color: #42b883;
+    }
+  }
+}
+</style>
